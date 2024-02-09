84dd2e9ed95af533eba9b4ee95d2c3bdd297a9e5
Chandra shekhar arya <66366710+shekhar613@users.noreply.github.com>
2024-02-09 13:31:49 +0530

    Update settings.py

--- codeGpt/settings.py
+++ codeGpt/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h7km-y1mn6pzk#$1ziaj$s^j$h%=e*he+rax$-5ajg#1c1*zqe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.practcode.com','practcode.com','*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
